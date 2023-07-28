b57c0887505fd12f92c8f542e1eabf173a76bfd6
RyanHaniff <haniffryan.hr@gmail.com>
2023-07-27 21:21:52 -0400

    added bas tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"], // asterix looks for all html in the build dir
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
